bbc065fa3c015948a38eafd8a3ba927b3ace2571
José Victor Persset <victor.persset@hotmail.com>
2025-01-27 16:35:11 -0300

    Add: create videogame model

---
+++ WebApiReviewDotNet9/Models/VideoGame.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApiReviewDotNet9.Models
+{
+    public class VideoGame
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Plataform { get; set; }
+        public string? Developer { get; set; }
+        public string? Publisher { get; set; }
+    }
+}

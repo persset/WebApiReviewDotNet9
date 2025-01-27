25bd7438ddcce0a35ba4d8deb6166169655c436a
José Victor Persset <victor.persset@hotmail.com>
2025-01-27 16:45:07 -0300

    Add: create videogamecontroller

---
+++ WebApiReviewDotNet9/Controllers/VideoGameController.cs
@@ -0,0 +1,45 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiReviewDotNet9.Models;
+
+namespace WebApiReviewDotNet9.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoGameController : ControllerBase
+    {
+        static private List<VideoGame> videoGames = new List<VideoGame>
+        {
+            new VideoGame
+            {
+                Id = 1,
+                Title = "Monster Hunter",
+                Platform = "PS2",
+                Developer = "Capcom",
+                Publisher = "Capcom"
+            },
+            new VideoGame
+            {
+                Id = 2,
+                Title = "The Legend of Zelda",
+                Platform = "NES",
+                Developer = "Nintendo",
+                Publisher = "Nintendo"
+            },
+            new VideoGame
+            {
+                Id = 3,
+                Title = "Yakuza Kiwami",
+                Platform = "PC",
+                Developer = "Ryu Ga Gotoku Studio",
+                Publisher = "Sega"
+            }
+        };
+        [HttpGet]
+        public ActionResult<List<VideoGame>> GetVideoGames()
+        {
+            return Ok(videoGames);
+        }
+    }
+}
--- WebApiReviewDotNet9/Models/VideoGame.cs
+++ WebApiReviewDotNet9/Models/VideoGame.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string? Title { get; set; }
-        public string? Plataform { get; set; }
+        public string? Platform { get; set; }
         public string? Developer { get; set; }
         public string? Publisher { get; set; }
     }
